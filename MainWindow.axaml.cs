3e33fefc4e19f8807e806acb8a35d4aba3ec8b3f
Gawasna <hunglepy05@gmail.com>
2026-03-23 00:34:56 +0700

    Initial commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace m_mslc_overlay
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
